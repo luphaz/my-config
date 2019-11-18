1b3cce31a77780bb0db8d2437f56af8106933b06
luphaz <luphaz@users.noreply.github.com>
2019-11-18 22:16:50 +0100

    No more hidden files and goodies
    * In order to simplify listing of repo
    * Let's get rid of `.`
    
    Goodies added :
    * `kj` kill all jobs
    * `i` is now isopod
    * `ist` is now istioctl
    * `v` is now vim
    * `diff` use git as default diff tool for coloring purpose
    * `gpro` opens pr for current branch
    * `grom` now reset to `origin/master`
    * `gdom` git diff from `origin/master`
    * `grbom` rebase to `origin/master`
    * `gromh` reset origin/master hard

--- setup.zsh
+++ setup.zsh
@@ -7,22 +7,22 @@ echo "BEGIN SETUP ZSH "
 abs_zsh=$(pwd)/zsh
 
 echo "-----------------------------------------------------------------------"
-echo "symlink zsh/.aliases to ~/.aliases"
+echo "symlink zsh/aliases to ~/.aliases"
 [  -d ~/.aliases ] && [ ! -h ~/.aliases ] && mv ~/.aliases ~/.aliases.backup
-[ ! -h ~/.aliases ] && ln -s $abs_zsh/.aliases ~/.aliases
+[ ! -h ~/.aliases ] && ln -s $abs_zsh/aliases ~/.aliases
 echo "~/.aliases is linked to"
 readlink ~/.aliases
 
 echo "-----------------------------------------------------------------------"
 echo "symlink zsh/.zshrc to ~/zshrc"
 [ ! -h ~/.zshrc ] && [ -f ~/.zshrc ] && mv ~/.zshrc ~/.zshrc.backup
-[ ! -h ~/.zshrc ] && ln -s $abs_zsh/.zshrc ~/.zshrc
+[ ! -h ~/.zshrc ] && ln -s $abs_zsh/zshrc ~/.zshrc
 echo "~/.zshrc is linked to"
 readlink ~/.zshrc
 
 echo "-----------------------------------------------------------------------"
 echo "symlink(s) zsh/.scripts/* to /usr/local/bin/."
-for file in $abs_zsh/.scripts/*(.); echo "symlink $file to /usr/local/bin/${file:t:r}" \
+for file in $abs_zsh/scripts/*(.); echo "symlink $file to /usr/local/bin/${file:t:r}" \
     && [ ! -h /usr/local/bin/${file:t:r} ] \
     && ln -s $file /usr/local/bin/${file:t:r}
 
--- zsh/aliases/lazy.zsh
+++ zsh/aliases/lazy.zsh
@@ -9,20 +9,43 @@ alias ez="code ~/.zshrc ~/.aliases/"
 # Used to open wercker into browser for current repo
 ow() {open "https://app.wercker.com/ricardo-ch/$(basename `git rev-parse --show-toplevel`)/runs"}
 
+# Kill all jobs in background using a do while like loop
+kj() { 
+  jobs
+  kill %
+  result=$?
+  while [[ $result == 0 ]]
+  do
+    kill % 
+    result=$?
+    echo last command result is $result
+    sleep 1
+  done
+}
+
 # Used to run minikube shortly
 alias mk=minikube
 
 # Used to run kubectl shortly
 alias k=kubectl
 
-# Used to run istio shortly 
-alias i=istioctl
+# Used to run isopod shortly 
+alias i=isopod
+
+# Used to run istioctl shortly 
+alias ist=istioctl
 
 # Used to run docker shortly
 alias d=docker
 
 # Used to run git shortly
 alias g=git
+
+# User to run vim shortly
+alias v=vim
+
+# Because colored git diff is way better that default diff available on MacOS
+alias diff="git diff"
 
 # Used to create a github pull request
 alias gpr="hub pull-request"
@@ -33,8 +56,20 @@ alias gprf="hub pull-request --no-edit"
 # Create a pull request using last commit message
 alias gprl='hub pull-request -m "$(git log -1 --pretty=%B)"'
 
+# Open last pull request of current project
+alias gpro='hub pr show'
+
 # Used to rebase origin/master into current branch
-alias grom="git rebase origin/master"
+alias grbom="git rebase origin/master"
+
+# Used to git diff origin/master
+alias gdom="git diff origin/master"
+
+# Used to reset from origin/master
+alias grom="git reset origin/master"
+
+# Used to reset from origin/master HARD
+alias gromh="git reset origin/master --hard"
 
 # Go to ricardo go path
 alias cdgo="cd $GORIC"
@@ -44,6 +79,12 @@ alias kg="kubectl get"
 
 # Used to get all pods
 alias kgp="kubectl get pods -o wide"
+
+# Used to get deployment spec as yaml
+alias kgy="kubectl get deploy -oyaml"
+
+# Used to get deployment spec as yaml for all namespaces
+alias kgya="kgy --all-namespaces"
 
 # Used to shorten the port forward command :
 alias kpf="kubectl port-forward"
